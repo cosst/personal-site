c9c8bcd54208e76797d2c9e7ba63999d2cd984d2
Nathaniel Schier <nathaniel.schier@gmail.com>
2018-03-05 22:13:54 -0800

    Update PDF file URL and webpack base config settings.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -64,14 +64,6 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      },
-      {
-        test: /\.(pdf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('[name].[hash:7].[ext]')
-        }
       }
     ]
   },
